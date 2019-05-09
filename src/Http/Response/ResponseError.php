5f284926ffc6f1e36459f3f1e21ca5a6006dff1c
HyanCat <hyancat.cn@gmail.com>
2019-05-09 20:50:39 +0800

    Add psr-7

--- src/Http/Response/ResponseError.php
+++ src/Http/Response/ResponseError.php
@@ -11,29 +11,56 @@ namespace Ruogu\Foundation\Http\Response;
 
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Jsonable;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Standard error response.
  * Class ResponseError
  * @namespace Ruogu\Foundation\Response
  */
-class ResponseError implements Arrayable, Jsonable
+class ResponseError implements Arrayable, Jsonable, ResponseInterface
 {
     protected $code;
     protected $message;
 
     public function __construct(int $code, string $message)
     {
-        $this->code    = $code;
+        $this->code = $code;
         $this->message = $message;
     }
 
+    /**
+     * @return int
+     * @deprecated
+     */
     public function getCode()
     {
         return $this->code;
     }
 
+    /**
+     * @return string
+     * @deprecated
+     */
     public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->code;
+    }
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        $new = clone $this;
+        $new->code = $code;
+        $new->message = $reasonPhrase;
+        return $new;
+    }
+
+    public function getReasonPhrase()
     {
         return $this->message;
     }
