6fd1c44ccb2c76a8cf75d81b48f7adb2ef461cf6
HyanCat <hyancat.cn@gmail.com>
2025-08-09 17:14:04 +0800

    Upgrade Laravel 11

--- src/helpers.php
+++ src/helpers.php
@@ -8,7 +8,6 @@
  */
 
 use Illuminate\Support\Str;
-use Webmozart\PathUtil\Path as P;
 
 if (!function_exists('is_not_null')) {
     function is_not_null($var): bool
@@ -33,10 +32,6 @@ if (!function_exists('is_production')) {
 
 function RGClientIP()
 {
-    // IP fields for aliyun slb.
-    // Request::setTrustedProxies(['100.97.0.0/16']);
-    // Fixed: 配置过可信任代理，这里就不需要阿里云 ip
-
     return request()->ip();
 }
 
@@ -53,14 +48,12 @@ function RGSchoolLevel($level)
 function RGUserLevelColor($level)
 {
     $levels = config('level.user');
-
     return $levels[$level % count($levels)]['color'];
 }
 
 function RGSchoolLevelColor($level)
 {
     $levels = config('level.school');
-
     return $levels[$level % count($levels)]['color'];
 }
 
@@ -81,10 +74,10 @@ function RGResponse($message = 'ok', $option = null)
 
 function RGError($code = 0, $message = 'ok', $option = null)
 {
-    return (new Ruogu\Foundation\Error\Error($code, $message, $option))->show();
+    return (new \Ruogu\Foundation\Error\Error($code, $message, $option))->show();
 }
 
-function RGException(Exception $e)
+function RGException(\Exception $e)
 {
     return RGError($e->getCode(), $e->getMessage());
 }
@@ -104,18 +97,15 @@ function RGImage($url, $style = null)
     if (null !== $style && Str::contains($url, 'o.ruogoo.cn')) {
         return str_replace('o.ruogoo.cn', 'i.ruogoo.cn', $url) . '@!' . $style;
     }
-
     return $url;
 }
 
 function rg_closure($closure): bool
 {
-    if ($closure instanceof Closure) {
+    if ($closure instanceof \Closure) {
         $closure();
-
         return true;
     }
-
     return false;
 }
 
@@ -133,11 +123,6 @@ function user_event(string $eventName, array $properties = [])
     $event->fire();
 }
 
-/**
- * @param      $var
- * @param null $property
- * @return bool|mixed
- */
 function invert(&$var, $property = null)
 {
     if (null === $property) {
@@ -145,48 +130,28 @@ function invert(&$var, $property = null)
     } else {
         $var->$property = !$var->$property;
     }
-
     return $var;
 }
 
-/**
- * @param $var
- * @return bool
- */
 function RGNull($var)
 {
     return null === $var;
 }
 
-/**
- * @param $var
- * @return bool
- */
 function RGNotNull($var)
 {
     return null !== $var;
 }
 
-/**
- * @param $closure
- * @return bool
- */
 function RGClosure($closure): bool
 {
-    if ($closure instanceof Closure) {
+    if ($closure instanceof \Closure) {
         $closure();
-
         return true;
     }
-
     return false;
 }
 
-/**
- * @param      $var
- * @param null $property
- * @return bool
- */
 function RGInvert(&$var, $property = null)
 {
     if (null === $property) {
@@ -194,17 +159,15 @@ function RGInvert(&$var, $property = null)
     } else {
         $var->$property = !$var->$property;
     }
-
     return $var;
 }
 
 function RGCdn($filePath)
 {
     if ($cdn = config('app.cdn')) {
-        return $cdn . P::makeAbsolute($filePath, '/');
+        return $cdn . '/' . ltrim($filePath, '/');
     }
-
-    return P::makeAbsolute($filePath, '/');
+    return '/' . ltrim($filePath, '/');
 }
 
 function RGRoute(string $name, $parameters = []): string
@@ -213,11 +176,9 @@ function RGRoute(string $name, $parameters = []): string
         return '';
     }
     $url = route($name, $parameters, true);
-
     if (app()->environment() === 'production') {
         return str_replace('http://', 'https://', $url);
     }
-
     return $url;
 }
 
